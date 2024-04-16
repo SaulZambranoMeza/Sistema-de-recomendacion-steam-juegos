9dde362c730bb2cb561df8354a0a41086221a9e1
Saul Zambrano Meza <99093279+SaulZambranoMeza@users.noreply.github.com>
2024-04-16 14:23:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -24,18 +24,18 @@ def developer(desarrollador: str):
     df_agrupado = df_dev.groupby(["year"], as_index=False).agg({"item_id":"count"})
     df_agrupado = df_agrupado.rename(columns={"item_id":"Cantidad de Items"})
 
-    #Agrupar por año y por si el item es free
+    #Se agrupan por año y por si el item es free
     df_agrupado_free = df_dev[df_dev['price'] == 'free'].groupby(["year"], as_index=False).agg({"item_id":"count"})
     df_agrupado_free = df_agrupado_free.rename(columns={"item_id": "free_items"})
 
-    #Combinar los resultados y calcular el porcentaje de items 'free' por año
+    #Se combinan los resultados y calcular el porcentaje de items 'free' por año
     df_agrupado = pd.merge(df_agrupado, df_agrupado_free, on="year", how="left")
     df_agrupado['Contenido Free'] = round(df_agrupado['free_items'] / df_agrupado['Cantidad de Items'] * 100, 2).fillna(0)
 
     #Eliminamos free_items
     df_agrupado = df_agrupado.drop("free_items", axis=1)
 
-    #Renombramos 'year' por 'Año'
+    #Se renombran 'year' por 'Año'
     df_agrupado = df_agrupado.rename(columns={"year":"Año"})
 
     #Convertimos a html
@@ -51,19 +51,21 @@ def str_to_float(value):      # metodo para sumar solo los valores numericos
 
 @app.get("/userdata/{user_id}")
 def userdata(user_id: str):
-    df_user = df_user_data[df_user_data["user_id"] == user_id]    #creo un df de solo el usuario buscado
-
+    #creo un df de solo el usuario buscado
+    df_user = df_user_data[df_user_data["user_id"] == user_id]    
     
-    dinero_gastado = df_user['price'].apply(str_to_float).sum()   #aplicando el metodo para sumar todo el dinero gastado
+    #Se aplica el metodo para sumar todo el dinero gastado
+    dinero_gastado = df_user['price'].apply(str_to_float).sum()   
+    #Obteniendo el verdadero valor de items (los que 'machean')
+    cantidad_items = len(df_user)                                 
+    #Creo df de reviews para el usuario buscado
+    df_reviews_user = df_reviews[df_reviews["user_id"] == user_id]   
+    total_reviews = len(df_reviews_user)                             
 
-    cantidad_items = len(df_user)                                 # obteniendo el verdadero valor de items (los que 'machean')
-
-    df_reviews_user = df_reviews[df_reviews["user_id"] == user_id]   # un df de reviews para el usuario buscado
-    total_reviews = len(df_reviews_user)                             # Obtenemos el total de reviews
-
-    cantidad_true = df_reviews_user["recommend"].sum()               # Obtenemos el total de reviews de recommend que son True
-
-    porcentaje_de_recomendacion = (cantidad_true * 100) / total_reviews # Obtenemos el porcentaje de recomendación
+    # Obtenemos el total de reviews de recommend que son True
+    cantidad_true = df_reviews_user["recommend"].sum()     
+    # Obtenemos el porcentaje de recomendación
+    porcentaje_de_recomendacion = (cantidad_true * 100) / total_reviews 
 
     respuesta = {"Usuario X": user_id, 
                  "Dinero gastado": dinero_gastado, 
@@ -79,27 +81,27 @@ def user_for_genre(genero: str):
     if 'genre' not in df_games_user_genre.columns:
         raise ValueError("El DataFrame df_games_copy no tiene una columna llamada 'genre'.")
 
-#Convertir la columna 'release_date' a tipo datetime
+#Se converte la columna 'release_date' a tipo datetime
     df_games_user_genre['release_date'] = pd.to_datetime(df_games_user_genre['release_date'], errors='coerce')
 
-#Filtrar df_games_copy por el género dado
+#Se filtra df_games_copy por el género dado
     juegos_genero = df_games_user_genre[df_games_user_genre['genre'] == genero]
 
-#Unir el DataFrame filtrado con df_user_items
+#Se unen el DataFrame filtrado con df_user_items
     juegos_usuario = pd.merge(df_items_user_genre, juegos_genero, on='item_id')
 
-#Calcular las horas jugadas por usuario para cada juego
+#Se calculan las horas jugadas por usuario para cada juego
     horas_por_usuario = juegos_usuario.groupby('user_id')['playtime_forever'].sum().reset_index()
 
-#Encontrar el usuario con más horas jugadas
+#Se busca el usuario con más horas jugadas
     usuario_max_horas = horas_por_usuario.loc[horas_por_usuario['playtime_forever'].idxmax()]['user_id']
 
-#Calcular la acumulación de horas jugadas por año de lanzamiento para el género dado
+#Calculamos la acumulación de horas jugadas por año de lanzamiento para el género dado
     horas_por_año = juegos_usuario.groupby(juegos_usuario['release_date'].dt.year)['playtime_forever'].sum().reset_index()
     horas_por_año.rename(columns={'playtime_forever': 'Horas'}, inplace=True)
     horas_por_año = horas_por_año.to_dict('records')
 
-#Crear el diccionario de resultados
+#Creamos el diccionario de resultados
     result = {
         "Usuario con más horas jugadas para {}: ".format(genero): usuario_max_horas,
         "Horas jugadas": horas_por_año
@@ -111,14 +113,14 @@ def user_for_genre(genero: str):
 @app.get("/top_desarrolladores/{year}",response_class=HTMLResponse)
 def best_developer_year(year: int):
     try:
-        # Filtrar por año y recomendaciones positivas
+        # Filtramos por año y recomendaciones positivas
         df_filtered = dfconcatenado[(dfconcatenado['year'] == year) & (dfconcatenado['recommend'] == True)]
 
-        # Contar la cantidad de recomendaciones por desarrollador
+        # Contamos la cantidad de recomendaciones por desarrollador
         df_counts = df_filtered.groupby('developer')['user_id'].count().reset_index()
         df_counts = df_counts.rename(columns={'user_id': 'cantidad_recomendaciones'})
 
-        # Ordenar por cantidad de recomendaciones y obtener el top 3
+        # Ordenamos por cantidad de recomendaciones y obtener el top 3
         df_top3 = df_counts.nlargest(3, 'cantidad_recomendaciones')
         df_html = df_top3.to_html(index=False)
         return df_html
@@ -130,7 +132,7 @@ def best_developer_year(year: int):
 def developer_reviews_analysis(developer: str):
     
     df_desarrollador = dfconcatenado[dfconcatenado['developer'] == developer]
-    # Filtrar registros con sentimiento negativo (valor 0) o positivo (valor 2)
+    #Se filtran registros con sentimiento negativo (valor 0) o positivo (valor 2)
     df_negativo = df_desarrollador[df_desarrollador['sentiment_analysis'] == 0]['user_id'].count()
     df_positivo = df_desarrollador[df_desarrollador['sentiment_analysis'] == 2]['user_id'].count()
 
@@ -144,7 +146,7 @@ def developer_reviews_analysis(developer: str):
     
     diccionario = dffila.to_dict(orient='records')
 
-    # Crear un diccionario con los resultados
+    # Creamos un diccionario con los resultados
     resumen_dict = {'developer':["Negative",df_negativo],"Positive":df_positivo}
     
     
